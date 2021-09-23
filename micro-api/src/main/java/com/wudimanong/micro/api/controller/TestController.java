f16f66893a673dc95c6eb069be556fcdf8e1cd3d
jiangqiao <jiangqiao@mafengwo.com>
2021-09-23 16:07:51 +0800

    升级版本

--- micro-api/src/main/java/com/wudimanong/micro/api/controller/TestController.java
+++ micro-api/src/main/java/com/wudimanong/micro/api/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wudimanong.micro.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author jiangqiao
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        log.info("测试接口被调用！");
         return "无依赖测试接口返回->OK!";
     }
 }
